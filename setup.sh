54cff51eea99537a5a83089f470d2b3ba0bc8acc
kura <akatoza@gmail.com>
2012-04-18 20:34:21 +0900

    しゅーせー

--- setup.sh
+++ setup.sh
@@ -7,11 +7,9 @@ else
   mkdir old_dotfiles
   mv ~/.vimrc old_dotfiles/vimrc
   mv ~/.zshrc old_dotfiles/zshrc
-  mv ~/.gitconfig old_dotfiles/gitconfig
   
   ln .vimrc ~/.vimrc
   ln .zshrc ~/.zshrc
-  ln .gitconfig ~/.gitconfig
 
 	echo "dotfiles 入れ替え完了。"
 	echo "以前の dotfiles は old_dotfiles ディレクトリに保管。"
