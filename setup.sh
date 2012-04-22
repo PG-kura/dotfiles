e6b66b6d6900aca1a4f5112ea793984044b54198
PG_kura <kura@higashi-hiroki-no-MacBook-Air.local>
2012-04-22 20:31:27 +0900

    add gitconfig

--- setup.sh
+++ setup.sh
@@ -6,8 +6,10 @@ then
 else
   mkdir old_dotfiles
   mv ~/.vimrc old_dotfiles/vimrc
+  mv ~/.gitconfig old_dotfiles/gitconfig
   
   ln .vimrc ~/.vimrc
+  ln .gitconfig ~/.gitconfig
 
 	echo "dotfiles 入れ替え完了。"
 	echo "以前の dotfiles は old_dotfiles ディレクトリに保管。"
