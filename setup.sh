d8409de46e4b0d38712c1fcd19091cf5aa9dbf50
PG_kura <kura@v156.dev.scaleout.jp>
2012-05-17 20:34:15 +0900

    tab -> space

--- setup.sh
+++ setup.sh
@@ -2,17 +2,17 @@
 
 if [ -d "old_dotfiles" ];
 then
-	echo "既に old_dotfiles があるので何もしない。"
+  echo "既に old_dotfiles があるので何もしない。"
 else
   mkdir old_dotfiles
   mv ~/.vimrc old_dotfiles/vimrc
   mv ~/.gitconfig old_dotfiles/gitconfig
-  
+
   ln .vimrc ~/.vimrc
   ln .gitconfig ~/.gitconfig
 
-	echo "dotfiles 入れ替え完了。"
-	echo "以前の dotfiles は old_dotfiles ディレクトリに保管。"
+  echo "dotfiles 入れ替え完了。"
+  echo "以前の dotfiles は old_dotfiles ディレクトリに保管。"
 fi
 
 cp -rf .vim ~/
